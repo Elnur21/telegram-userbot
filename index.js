eae42b681013afcde95ea39fd5d5e95f447138ee
Sandy Ken <tokairobo@gmail.com>
2023-07-25 00:52:42 +0700

    initial commit

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ require('dotenv/config')
 const fastify = require("fastify");
 //const { client, initialize } = require("./src/userbot");
 
+const port = process.env.PORT
 const app = fastify({ logger: false })
 
 app.get('/', async ( _, res ) => {
- // const status = await client.isUserAuthorized()
- // if ( status ) {
- //   await initialize()
- // }
+  //const status = await client.isUserAuthorized()
+  //if ( status ) {
+  //  await initialize()
+  //}
 
   res.send({
     ok: true,
-    is_connected: null
+    is_connected: status
   })
 })
 
-app.listen({ port: 3000 }, async ( err, address ) => {
+app.listen({ port: parseInt(port) }, ( err, address ) => {
   if ( err ) {
     return console.error(err)
   }
